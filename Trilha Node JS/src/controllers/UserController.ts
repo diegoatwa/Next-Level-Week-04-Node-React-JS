4c804aaef056e60d1accae3807427ce5093c9490
Diego Azevedo <contato.diegoazevedo@gmail.com>
2021-02-25 02:55:25 -0300

    day 3

--- Trilha Node JS/src/controllers/UserController.ts
+++ Trilha Node JS/src/controllers/UserController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express"
-import { User } from "../models/User"
-import { getRepository } from "typeorm"
+import { getCustomRepository } from "typeorm"
+import { UsersRepository } from "../repositories/UsersRepository"
+
 
 class UserController {
     async create(req: Request, res: Response){
         const { name, email } = req.body
-        const usersRepository = getRepository(User)
+        const usersRepository = getCustomRepository(UsersRepository)
         const usarAlreadyExists = await usersRepository.findOne({ email })
 
         if (usarAlreadyExists){
@@ -17,7 +18,7 @@ class UserController {
         const user = usersRepository.create({ name, email })
         await usersRepository.save(user)
         
-        return res.json(user)
+        return res.status(201).json(user)
     }
 }
 
--- Trilha Node JS/src/routes.ts
+++ Trilha Node JS/src/routes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express"
+import { SurveyController } from "./controllers/SurveysController"
 import { UserController } from "./controllers/UserController"
 
 const router = Router()
 
 const userController = new UserController()
-
 router.post("/users", userController.create)
+
+const surveysController = new SurveyController()
+router.post('/surveys', surveysController.create)
+
+
+router.get('/surveys', surveysController.show)
+
+
 
 export { router }
